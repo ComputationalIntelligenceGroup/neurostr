b906a5f2ca5d5c40a23be4659721894296dcb199
bmihaljevic <bmihaljevic@fi.upm.es>
2018-03-27 16:51:33 +0200

    node features extract fix minor

--- tools/NodeFeatureExtractor.cpp
+++ tools/NodeFeatureExtractor.cpp
@@ -4,8 +4,9 @@
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/variables_map.hpp>
 
-#include <neurostr/tools/node_features.h>
+#include <neurostr/tools/node_features.h> 
 
+namespace po = boost::program_options;
 
 /**
  * @brief 
